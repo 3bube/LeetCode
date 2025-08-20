8d798a17d76445dcea5db774458117edf660e899
Ebubechukwu Ugbor <ugbore2@gmail.com>
2025-08-20 23:00:24 +0100

    solved jump game

---
+++ Medium/Jump Game.py
@@ -0,0 +1,8 @@
+def canJump(self, nums):
+    maxReach = 0
+
+    for i in range(len(nums)):
+        if i > maxReach:
+            return False
+        maxReach = max(maxReach, i + nums[i])
+    return True
